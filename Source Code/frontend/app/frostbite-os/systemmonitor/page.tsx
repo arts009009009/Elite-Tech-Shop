d5ea80f422fc94253c8e45304c39817dfebba62c
arts009009009 <arts009009009@Outlook.com>
2026-08-27 16:52:52 +0300

    System Monitor
    Page.tsx for Monitor

--- Source Code/frontend/app/frostbite-os/systemmonitor/page.tsx
+++ Source Code/frontend/app/frostbite-os/systemmonitor/page.tsx
@@ -1,1 +1,12 @@
+"use client";
 
+import dynamic from "next/dynamic";
+
+const SystemMonitor = dynamic(
+  () => import("@/components/frostbite-os/SystemMonitor"),
+  { ssr: false }
+);
+
+export default function SystemMonitorPage() {
+  return <SystemMonitor asApp />;
+}
