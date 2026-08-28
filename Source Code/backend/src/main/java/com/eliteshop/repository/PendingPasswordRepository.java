5b9b75afbade06149c5a750d3fe255d2b7b0579a
arts009009009 <arts009009009@Outlook.com>
2026-08-28 19:12:50 +0300

    Added files

--- Source Code/backend/src/main/java/com/eliteshop/repository/PendingPasswordRepository.java
+++ Source Code/backend/src/main/java/com/eliteshop/repository/PendingPasswordRepository.java
@@ -3,9 +3,11 @@ package com.eliteshop.repository;
 import com.eliteshop.model.PendingPasswordDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
 import java.util.Optional;
 
 public interface PendingPasswordRepository extends MongoRepository<PendingPasswordDocument, String> {
     Optional<PendingPasswordDocument> findByEmail(String email);
     void deleteByEmail(String email);
+    void deleteByCreatedAtBefore(Date date);
 }
