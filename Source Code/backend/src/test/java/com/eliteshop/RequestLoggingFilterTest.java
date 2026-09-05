ccfa20b648403871bb4ce37ac22acd5d82c1bcde
arts009009009 <arts009009009@Outlook.com>
2026-09-05 20:25:50 +0300

    backend/src/main/java/com/eliteshop/
    deleted 16 duplicate files

--- Source Code/backend/src/test/java/com/eliteshop/RequestLoggingFilterTest.java
+++ Source Code/backend/src/test/java/com/eliteshop/RequestLoggingFilterTest.java
@@ -1,6 +1,6 @@
 package com.eliteshop;
 
-
+import com.eliteshop.config.RequestLoggingFilter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
